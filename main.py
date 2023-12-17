16f71e3c97e24c35c1cfa874ca3bd5e06e62a9a4
Hidden eye <eye.hidden@yandex.com>
2023-12-17 23:29:17 +0600

    make it better

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
+import os
 import pickle as pk
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 import numpy as np
 
-model = pk.load(open(r'C:\Users\Anik\Documents\model','rb'))
+# Use absolute path to the model file
+model_path = os.path.abspath('C:\\Users\\Anik\\PycharmProjects\\ML\\models\\model')
+
+# Check if the file exists
+if not os.path.exists(model_path):
+    raise FileNotFoundError(f"Model file not found at: {model_path}")
+
+model = pk.load(open(model_path, 'rb'))
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return "Hello world"
 
-@app.route('/predict',methods =['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    sex = request.form.get('sex')
-    age = request.form.get('age')
-    hypertension = request.form.get('hypertension')
-    disease = request.form.get('disease')
-    work = request.form.get('work')
-    glucose = request.form.get('glucose')
-    bmi = request.form.get('bmi')
-    smoking = request.form.get('smoking')
-    input_query = np.array([[sex,age,hypertension,disease,work,glucose,bmi,smoking]])
+    sex = int(request.form.get('sex'))
+    age = int(request.form.get('age'))
+    hypertension = int(request.form.get('hypertension'))
+    disease = int(request.form.get('disease'))
+    work = int(request.form.get('work'))
+    glucose = float(request.form.get('glucose'))
+    bmi = float(request.form.get('bmi'))
+    smoking = int(request.form.get('smoking'))
+
+    input_query = np.array([[sex, age, hypertension, disease, work, glucose, bmi, smoking]])
     result = model.predict(input_query)[0]
-    return jsonify({'placement':str(result)})
+
+    return jsonify({'placement': str(result)})
+
 if __name__ == '__main__':
     app.run(debug=True)
